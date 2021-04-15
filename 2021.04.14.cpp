2e3bc1adfafac9cc26b447d9af926f41f4eb3cce
adminvae <57033937+adminvae@users.noreply.github.com>
2021-04-15 11:52:03 +0800

    Add files via upload

---
+++ 2021.04.14.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+//转换以下ASCII码为对应字符并输出他们
+//73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116, 33
+//输出结果 I can do it ！
+//int main(){
+//		printf("%c%c%c%c%c%c%c%c%c%c%c%c\n", 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116, 33);
+//		system("pause");
+//		return 0;
+//}
+//一行输入一个人的出生日期（包括年月日），将该生日中的年、月、日分三行输出
+//int main(){
+//	int year;
+//	int month;
+//	int day;
+//	scanf("%4d%2d%2d", &year, &month, &day);
+//	printf("year=%d\n", year);
+//	printf("month=%02d\n", month); //使用%0d输出数值时指定左面不使用的空位置自动填0
+//	printf("date=%02d\n", day);
+//	system("pause");
+//	return 0;
+//}
+//输入两个整数，范围-231~231-1，交换两个数并输出。
+//int main(){
+//		int a = 0;
+//		int b = 0;
+//		scanf("a=%d,b=%d", &a, &b);
+//		printf("a=%d,b=%d\n", b, a);
+//		system("pause");
+//		return 0;
+//}
+//输入一个字符，输出该字符相应的ASCII码
+//int main(){
+//	char a;
+//	scanf("%c", &a);
+//	printf("%d\n", a);
+//	system("pause");
+//	return 0;
+//}
