6b385a9959ab05fa670c708e246f5d653e192150
adminvae <57033937+adminvae@users.noreply.github.com>
2021-05-16 15:47:06 +0800

    Add files via upload

---
+++ 2021.05.13.c
@@ -0,0 +1,39 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include <string.h>
+//#include <windows.h>
+//#include<assert.h>
+//#include"heap.h"
+//void  HeapSort(int* a,int n) //¶ÑÅÅÐò ½µÐò½¨Ð¡¶Ñ
+//{
+//	for (int i = (n - 2) / 2; i >= 0;--i)
+//	{
+//		AdjustDown(a, n, i);
+//	}
+//	for (int i = 0; i<n;++i)
+//	{
+//		printf("%d", a[i]);
+//	}
+//	printf("\n");
+//	int end = n - 1;
+//	while (end>0)
+//	{
+//		int tmp = a[end];
+//		a[end] = a[0];
+//		a[0] = tmp;
+//		for (int i = 0; i < n; ++i)
+//		{
+//			printf("%d", a[i]);
+//		}
+//		AdjustDown(a, end, 0);
+//		--end;
+//	}
+//}
+//int main(){
+//	int a[10] = { 27, 25, 19, 34, 65, 37, 25, 49, 28, 18 };
+//	HeapSort(a, 10);
+//	system("pause");
+//	return 0;
+//}
+
